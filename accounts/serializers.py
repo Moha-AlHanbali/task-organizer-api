8be5c1122d8c8f74bb3c3c86b10c59d6b6bffb80
Moha-AlHanbali <moha.alhanbali@gmail.com>
2022-01-23 12:51:54 +0200

    fix: token data and date serialization

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,7 @@
 
+import json
+import datetime
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
@@ -10,9 +13,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
         token['username'] = user.username
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['age'] = json.dumps(user.age.__str__())
         return token
 
 class AccountSerializer(serializers.ModelSerializer):
--- task_organizer/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-from .serializers import MyTokenPairSerializer
-
-class TokenObtainedPairCustomView(TokenObtainPairView):
-    serializer_class = MyTokenPairSerializer
